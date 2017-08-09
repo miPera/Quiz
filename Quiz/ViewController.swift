9e0f15e8767c536dcaf578d4e03f63eafa9f172d
miPera <migperalta10@gmail.com>
2017-08-08 21:00:47 -0700

    Added comments

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -1,8 +1,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var questionLabel: UILabel!
-    @IBOutlet var answerLabel: UILabel!
+    @IBOutlet var questionLabel: UILabel!   // Outlet to Next Question UILabel
+    @IBOutlet var answerLabel: UILabel!     // Outlet to Show Answer UILabel
     
     let questions: [String] = [
         "What is 7+7?",
@@ -18,11 +18,13 @@ class ViewController: UIViewController {
     
     var currentQuestionIndex: Int = 0;
     
+    // show initial question on view load
     override func viewDidLoad() {
         super.viewDidLoad()
         questionLabel.text = questions[currentQuestionIndex]
     }
     
+    // Action method to show the next question
     @IBAction func showNextQuestion(_ sender: UIButton) {
         currentQuestionIndex += 1
         
@@ -35,6 +37,7 @@ class ViewController: UIViewController {
         answerLabel.text = "???"
     }
     
+    // Action method to show answer to current question
     @IBAction func showAnswer(_ sender: UIButton) {
         let answer: String = answers[currentQuestionIndex]
         answerLabel.text = answer
